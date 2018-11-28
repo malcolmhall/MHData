1eb4ab1f903443203bfb5994f09c7d97eb0ca218
Malcolm Hall <malhal@users.noreply.github.com>
2018-11-28 15:48:30 +0000

    Moved the master support, added foundation

--- MCoreData/MCoreData.h
+++ MCoreData/MCoreData.h
@@ -12,7 +12,6 @@
 #import <MCoreData/MCDStackManager.h>
 #import <MCoreData/MCDPersistentStoreBridge.h>
 #import <MCoreData/MCDFetchedTableDataSource.h>
-#import <MCoreData/MCDFetchedTableDataSourceMasterSupport.h>
 #import <MCoreData/MCDTableViewCellObject.h>
 #import <MCoreData/MCDPersistentContainer.h>
 #import <MCoreData/MCDNSPersistentContainer.h>
